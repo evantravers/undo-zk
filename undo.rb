9b0e3b3e9d5065cecdd630378c0972bf331963db
Evan Travers <evantravers@gmail.com>
2021-02-23 10:56:37 -0600

    First working version
    - is able to redirect links
    - is able to put certain notes in folders

--- undo.rb
+++ undo.rb
@@ -1,10 +1,24 @@
 # Flipping my ZK back to Obsidian's Format
+# - make a map of filename transitions
+#     - Use the `title:` attribute for the filename
+#     - Rules
+#         - `tag:#journal` -> Daily note in `/journal`
+#         - `tag:#career` -> Folders `/careers/eab/` etc.
+#         - `tag:#booknotes` -> Folder: `/booknotes
+#     - Error any duplicate final filenames
+# - Change Links
+#     - For each starting, filename, search through the ZK to find matching
+#       links, replacing with the final filename.
+#     - Leave block references and header references alone.
+# - Execute
+#     - Have a dry-run option
+#     - Logging
 
 require 'pry'
 
 SRC = "#{Dir.home}/Dropbox/wiki"
 DST = './wiki'
-FORBIDDEN = ['<', '>', ':', '"', '/', '\\', '|', '?', '*']
+FORBIDDEN = /[<>:"\/\\|?*]/
 
 class Zettel
   attr_reader :content
@@ -15,52 +29,80 @@ class Zettel
 
   def tags
     match = /^tags: ?\[(.*)\]$/.match(@content)
-      if match
-        match
-        .captures
-        .first
-        .split(/, ?/)
+
+    if match
+      match
+      .captures
+      .first
+      .split(/, ?/)
+      .map { |t| t.gsub('"', '') }
+    else
+      return nil
+    end
+  end
+
+  def folders
+    if tags
+      case
+      when tags.include?('#booknote')
+        ['booknotes']
+      when tags.include?('#links')
+        ['links']
       else
-        return nil
+        ""
       end
+    else
+      ""
+    end
+  end
+
+  def filename
+    title
   end
 
   def title
-    /^title: "?(.*)"?$/.match(@content).captures.first
+    title = /^title: "?(.*)"?$/.match(@content)
+
+    title.captures.first.gsub(FORBIDDEN, "")
   end
 end
-
-#     - Error any duplicate final filenames
-# - Change Links
-#     - For each starting, filename, search through the ZK to find matching
-#       links, replacing with the final filename.
-#     - Leave block references and header references alone.
-# - Execute
-#     - Have a dry-run option
-#     - Logging
-filenames = {}
 
 def err(str)
   puts str
   binding.pry
 end
 
-# delete the distribution folder
-Dir.each_child(DST) { |f| File.delete(File.join(DST, f)) }
+def fix_links(content, filenames)
+  filenames.reduce(content) do |new_content, mapping|
+    old, new = mapping
 
-# - make a map of filename transitions
-#     - Use the `title:` attribute for the filename
-#     - Rules
-#         - `tag:#journal` -> Daily note in `/journal`
-#         - `tag:#career` -> Folders `/careers/eab/` etc.
-#         - `tag:#booknotes` -> Folder: `/booknotes`
+    new_content.gsub(old, new)
+  end
+end
+
+# delete the distribution folder
+`rm -rf #{DST}`
+
+# ---
+
+# filenames becomes a mapping of [old => new] names
+filenames = {}
 Dir.glob("#{SRC}/*.md").each do |file|
   zettel = Zettel.new(file)
   err("🛑 duplicate name: #{file}") if filenames.values.include?(zettel.title)
 
-  filename = "#{zettel.title}.md"
-  content = zettel.content
-
-  File.write("#{DST}/#{filename}", content)
+  filenames[File.basename(file, ".*")] = zettel.filename 
 end
 
+# make files based on the new filenames, but fixing the links in the content
+Dir.glob("#{SRC}/*.md").each do |file|
+  zettel = Zettel.new(file)
+
+  # create any folders
+  `mkdir -p #{File.join(DST, zettel.folders)}`
+
+  File.write(
+    File.join([DST, zettel.folders, "#{zettel.filename}.md"]),
+    fix_links(zettel.content, filenames)
+  )
+end
